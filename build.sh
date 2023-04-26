e895d9fa42ca69e830c379e89d6e60d8f9ba131d
VARUN GAUR <101390136+VGAUR2003@users.noreply.github.com>
2023-04-26 10:02:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=azure
+COMPILER=cosmic
 
 ##----------------------------------------------------------##
 # Specify Linker
